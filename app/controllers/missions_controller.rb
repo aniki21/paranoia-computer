126330dac332c5e4aa19442cae2064edf1a804c1
Paul C <pacosgrove1@googlemail.com>
2017-04-02 22:13:56 +0100

    alert cancel; mobile menu; comments

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,6 +1,7 @@
 class MissionsController < ApplicationController
   
-  # /
+  # GET /
+  # GET /mission/:id
   def show
     @mission_id = params[:m].present? ? params[:m].to_i : 1
     @generic_lines = VoiceLine.generic
