467d2874bc1e7f9de7cbea9e3fe51931536fad00
Paul C <pacosgrove1@googlemail.com>
2017-04-02 21:28:43 +0100

    restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :missions, only: [:show]
 
-  resources :voice_lines do
+  resources :voice_lines, only: [:index,:create,:update,:destroy] do
     collection do
       get 'say', to: 'voice_lines#say'
     end
