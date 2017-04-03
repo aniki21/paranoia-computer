10b1b915f08c2d94a67c4140959acc53b7e71455
Paul C <paul@yoyogames.com>
2017-04-03 16:28:19 +0100

    write out voice lines

--- lib/voice.rb
+++ lib/voice.rb
@@ -4,4 +4,10 @@ class Voice
     %x(espeak -p 70 -s 160 -ven-rp+m7  "#{message}")
   end
 
+
+  def self.write(message,filename=nil)
+    filename ||= File.join(message.parameterize,".wav")
+    %x(espeak -p 70 -s 160 -ven-rp+m7 -w #{File.join(Rails.root,"public","clips",filename)} "#{message}")
+  end
+
 end
